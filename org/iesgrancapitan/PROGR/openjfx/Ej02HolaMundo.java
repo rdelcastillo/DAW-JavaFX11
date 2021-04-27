9b75ef295ee300d910377357fecb8846332a0770
Rafael del Castillo Gomariz <rafa.castillo@gmail.com>
2021-04-27 23:24:19 +0200

    Ejemplo de GUI usando JavaFX con Stage y Scene

--- org/iesgrancapitan/PROGR/openjfx/Ej02HolaMundo.java
+++ org/iesgrancapitan/PROGR/openjfx/Ej02HolaMundo.java
@@ -17,14 +17,14 @@ public class Ej02HolaMundo extends Application {
   @Override
   public void start(Stage primaryStage) {
     primaryStage.setTitle("Mi primera aplicación JavaFX");
-    
+
     // para visualizar contenido en la ventana de la aplicación
     // hay que añadir un componente a un objeto Scene, y este al
     // objeto Stage
     Label label = new Label("¡Hola Mundo, JavaFX!");
     Scene scene = new Scene(label, 400, 200);
     primaryStage.setScene(scene);
-    
+
     primaryStage.show();
   }
 
