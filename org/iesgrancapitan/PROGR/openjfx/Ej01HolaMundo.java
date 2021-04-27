042d85b60d8b0ed2055aad75e01093f230bf1e36
Rafael del Castillo Gomariz <rafa.castillo@gmail.com>
2021-04-27 18:58:55 +0200

    Ejemplo de GUI usando JavaFX

--- org/iesgrancapitan/PROGR/openjfx/Ej01HolaMundo.java
+++ org/iesgrancapitan/PROGR/openjfx/Ej01HolaMundo.java
@@ -1,5 +1,7 @@
 package org.iesgrancapitan.PROGR.openjfx;
 
+import java.util.Scanner;
+
 /**
  * Ejemplo de GUI usando JavaFX.
  * 
@@ -14,10 +16,29 @@ public class Ej01HolaMundo extends Application {
 
   @Override
   public void start(Stage primaryStage) {
+    System.out.println("Soy el método start(), conmigo empieza la interfaz gráfica.\n");
     
-    // el objeto stage que se recibe es creado por Application
+    // el objeto stage (ventana) que se recibe es creado por Application
     primaryStage.setTitle("Mi primera aplicación JavaFX");
     primaryStage.show();
+  }
+  
+  public void init() {
+    muestraMensajeInit();
+  }
+  
+  public void stop() {
+    System.out.println("Soy el método stop(), no soy obligatorio y conmigo terminamos.");
+  }
+
+  private void muestraMensajeInit() {
+    Scanner s = new Scanner(System.in);
+    System.out.println("Soy el método init() y si aparezco (no soy obligatorio), "
+        + "soy llamado el primero y antes de que la interfaz gráfica se inicie.");
+    System.out.println("Normalmente soy útil cuando es necesario inicializar "
+        + "rutinas antes del lanzamiento de la GUI.\n");
+    System.out.println("Pulsa Intro para continuar...");
+    s.nextLine();
   }
 
   public static void main(String[] args) {
