83ea40baeb7b9bc0aa58a2f8b0798fc22cc2f70c
Rafael del Castillo Gomariz <rafa.castillo@gmail.com>
2021-04-28 12:08:37 +0200

    Ejemplos de ventanas con diferentes modalidades

--- org/iesgrancapitan/PROGR/openjfx/Ej03Ventanas.java
+++ org/iesgrancapitan/PROGR/openjfx/Ej03Ventanas.java
@@ -7,6 +7,8 @@ package org.iesgrancapitan.PROGR.openjfx;
  */
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
@@ -18,23 +20,28 @@ public class Ej03Ventanas extends Application {
   @Override
   public void start(Stage primaryStage) {
     primaryStage.setTitle("JavaFX App");
-
-    Stage stage = new Stage();
     
-    //stage.initModality(Modality.APPLICATION_MODAL); // bloquea todas las ventanas
+    Stage stage1 = new Stage();
+    stage1.setTitle("Soy una ventana secundaria");
+    stage1.setScene(new Scene(new Button("Soy un humilde Button")));
     
-    stage.initOwner(primaryStage);
-    stage.initModality(Modality.WINDOW_MODAL); // bloquea la ventana principal (dueña de este escenario)
-    
-    //stage.initModality(Modality.NONE);
-    
-    stage.setTitle("Modo ventana: " + stage.getModality());
+    Stage stage2 = new Stage();
+    stage2.setX(300);
+    stage2.setY(300);
+    stage2.initOwner(primaryStage);
+    stage2.initModality(Modality.WINDOW_MODAL); // bloquea la ventana principal (dueña de este escenario)
+    //stage2.initModality(Modality.APPLICATION_MODAL); // bloquea todas las ventanas
+    //stage2.initModality(Modality.NONE);
+    stage2.setTitle("Modo ventana: " + stage2.getModality());
 
     primaryStage.show();
-    System.out.println("Hemos lanzado el 'Stage' primario (pero no cerrado).");
+    System.out.println("Hemos lanzado el 'Stage' primario.");
+    
+    stage1.show();
+    System.out.println("Hemos lanzado el 'Stage' secundario.");
 
-    stage.showAndWait();
-    System.out.println("Hemos cerrado el 'Stage' secundario.");
+    stage2.showAndWait();
+    System.out.println("Hemos cerrado el 'Stage' " + stage2.getTitle());
 
   }
 }
