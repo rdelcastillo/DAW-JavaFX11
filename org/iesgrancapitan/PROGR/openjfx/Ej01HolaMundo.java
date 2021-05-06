62f6e66d4b2723b3a73d5c5bdae7c597443fd61f
Rafael del Castillo Gomariz <rafa.castillo@gmail.com>
2021-05-06 09:14:19 +0200

    Primer ejemplo de GUI usando JavaFX

--- org/iesgrancapitan/PROGR/openjfx/Ej01HolaMundo.java
+++ org/iesgrancapitan/PROGR/openjfx/Ej01HolaMundo.java
@@ -23,15 +23,10 @@ public class Ej01HolaMundo extends Application {
     primaryStage.show();
   }
   
+  /**
+   * Este método es llamado el primero, antes de que se cree la ventana principal. 
+   */
   public void init() {
-    muestraMensajeInit();
-  }
-  
-  public void stop() {
-    System.out.println("Soy el método stop(), no soy obligatorio y conmigo terminamos.");
-  }
-
-  private void muestraMensajeInit() {
     Scanner s = new Scanner(System.in);
     System.out.println("Soy el método init() y si aparezco (no soy obligatorio)");
     System.out.println("soy llamado el primero y antes de que la interfaz gráfica se inicie.");
@@ -40,6 +35,14 @@ public class Ej01HolaMundo extends Application {
     System.out.println("Pulsa Intro para continuar...");
     s.nextLine();
   }
+  
+  /**
+   * Este método se llama el último, después de que se hayan cerrado las ventanas.
+   */
+  public void stop() {
+    System.out.println("Soy el método stop(), no soy obligatorio y conmigo terminamos.");
+  }
+
 
   public static void main(String[] args) {
     launch(args);
