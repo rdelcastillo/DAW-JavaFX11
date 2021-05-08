86a4e1c99ede8150d9f875de65e585eef92949b5
Rafael del Castillo Gomariz <rafa.castillo@gmail.com>
2021-05-08 13:14:16 +0200

    Fichero module-info.java

--- module-info.java
+++ module-info.java
@@ -5,7 +5,11 @@ module hellofx {
   requires javafx.base;
   
   opens org.iesgrancapitan.PROGR.openjfx to javafx.fxml;
+  opens org.iesgrancapitan.PROGR.openjfx.vistas to javafx.fxml;
+  opens org.iesgrancapitan.PROGR.openjfx.ejercicios.vistas to javafx.fxml;
   
   exports org.iesgrancapitan.PROGR.openjfx;
   exports org.iesgrancapitan.PROGR.openjfx.dialogos;
+  exports org.iesgrancapitan.PROGR.openjfx.ejercicios;
+  exports org.iesgrancapitan.PROGR.openjfx.ejercicios.vistas;
 }
