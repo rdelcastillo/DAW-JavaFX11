d2319a5494caea000d05efdbe56a060333593b88
Rafael del Castillo Gomariz <rafa.castillo@gmail.com>
2021-05-31 12:43:53 +0200

    Fichero module-info.java

--- module-info.java
+++ module-info.java
@@ -9,6 +9,7 @@ module hellofx {
   opens org.iesgrancapitan.PROGR.openjfx.vistas to javafx.fxml;
   opens org.iesgrancapitan.PROGR.openjfx.ejercicios.vistas to javafx.fxml;
   opens org.iesgrancapitan.PROGR.openjfx.controles.view to javafx.fxml;
+  opens org.iesgrancapitan.PROGR.openjfx.examenes.vistas to javafx.fxml;
   
   exports org.iesgrancapitan.PROGR.openjfx;
   exports org.iesgrancapitan.PROGR.openjfx.controles;
@@ -18,4 +19,6 @@ module hellofx {
   exports org.iesgrancapitan.PROGR.openjfx.eventos;
   exports org.iesgrancapitan.PROGR.openjfx.ejercicios;
   exports org.iesgrancapitan.PROGR.openjfx.ejercicios.vistas;
+  exports org.iesgrancapitan.PROGR.openjfx.examenes;
+  exports org.iesgrancapitan.PROGR.openjfx.examenes.vistas;
 }
